cdecd1936c5db6f1be47a370318f81cb90ac31c1
MarinaKim <marinakim456@yandex.ru>
2024-09-27 20:51:23 +0800

    пр4 ревью 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,31 @@
 // @todo: Темплейт карточки
-  const placesList = document.querySelector('.places__list'); 
-  const cardTmp = document.querySelector('#card-template').content;
-  
+const cardTmp = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list'); 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 const createCard = function (name, link) {
 
-   const cardElement = cardTmp.querySelector('.card').cloneNode(true)
-   cardElement.querySelector('.card__title').textContent = name;
-   cardElement.querySelector('.card__image').textContent = link;
+  const cardElement = cardTmp.querySelector('.card').cloneNode(true)
 
-   return cardElement;
-   console.log('ddd')
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').textContent = 'alt';
+  
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteButtonFunction);
+   
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
-
 const deleteButtonFunction = function (event) {
-  const listItem = event.target.closest('.card');
-
-  listItem.remove();
-}
+    const listItem = event.target.closest('.card');
+    listItem.remove();
+  }
 // @todo: Вывести карточки на страницу
+
 initialCards.forEach((el)=> {
 
-  const cardElement = cardTmp.querySelector('.card').cloneNode(true)
-  cardElement.querySelector('.card__title').textContent = el.name;
-  cardElement.querySelector('.card__image').setAttribute('src', `${el.link}`);
-  cardElement.querySelector('.card__image').textContent = 'alt';
-  
-  placesList.appendChild(cardElement);
+  placesList.appendChild(createCard(el.name, el.link));
 
-  cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('card__like-button_is-active');
-  });
-  
-  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-
-  cardDeleteButton.addEventListener('click', deleteButtonFunction);
 });
